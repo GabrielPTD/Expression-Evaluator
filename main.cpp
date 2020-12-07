092b7fc138e8fa1141f97c420f866bc0fafebafd
Gabriel Constantin Postudor <postudorgabriel@outlook.com>
2020-12-07 14:55:03 +0200

    Added predefined values(pi, e, etc). Added support for variables. Added new operators.

--- main.cpp
+++ main.cpp
@@ -11,7 +11,12 @@
 std::vector<std::string> PostfixNotation;
 std::stack<long double> Results;
 
-std::map<std::string, short> Operators = { {"+", 1}, {"-", 1}, {"*", 2}, {"/", 2}, {"^", 3}, {"==", 4}, {"=", 5} };
+//std::map<std::string, short> Operators = { {"+", 1}, {"-", 1}, {"*", 2}, {"/", 2}, {"^", 3}, {"==", 4}, {"=", 5} };
+std::map<std::string, short> Operators = { {"^", 1}, {"+", 2}, {"-", 2}, {"*", 3}, {"/", 3}, {"\\", 4}, {"mod", 5}, {"=", 6}, {"<>", 6}, {"<", 6}, {"<=", 6},
+											{">", 6}, {">=", 6}, {"not", 7}, {"and", 8}, {"or", 9}, {"xor", 10}};
+
+std::map<std::string, long double> Predefined = { {"true", 1}, {"false", 0}, {"pi", 3.14159}, {"e", 2.71828} };
+std::map<std::string, long double> Variables;
 //===============Functions===============//
 short CheckOperator(const std::string& op) {
 	const auto& it = Operators.find(op);
@@ -22,8 +27,22 @@ short CheckOperator(const std::string& op) {
 	return 0;
 }
 
+bool GetVariable(const std::string& var, long double& X) {
+	const auto& it = Variables.find(var);
+
+	if (it != Variables.end()) {
+		X = it->second;
+		return true;
+	}
+
+	return false;
+}
+
+void AddVariable(const std::string& var, long double X) {
+	Variables.insert({var, X});
+}
 bool IsFunction(const std::string& op) {
-	if (op == "pow")
+	if (op == "pow" || op == "sin")
 		return true;
 
 	return false;
@@ -31,6 +50,29 @@ bool IsFunction(const std::string& op) {
 
 bool IsNumber(const std::string& op) {
 	return std::regex_match(op, std::regex(("((\\+|-)?[[:digit:]]+)(\\.(([[:digit:]]+)?))?")));
+}
+
+bool IsVariable(const std::string& op) {
+	if (!isalpha(op[0]))
+		return false;
+
+	for (size_t i = 1; i < op.length(); ++i)
+		if (!isalnum(op[i]))
+			return false;
+
+	return true;
+}
+
+bool IsPredefined(const std::string& op) {
+	return Predefined.find(op) != Predefined.end();
+}
+
+long double GetPredefinedValue(const std::string& op) {
+	const auto& it = Predefined.find(op);
+	if (it != Predefined.end())
+		return it->second;
+
+	return 0;
 }
 
 void GeneratePostfix(const std::string& expr) {
@@ -74,7 +116,18 @@ void GeneratePostfix(const std::string& expr) {
 			}
 			if (IsFunction(token))
 				ops.push(token);
-
+			else if ((priority = CheckOperator(token))) {
+				while (!ops.empty() && ops.top() != "(" && (CheckOperator(ops.top()) >= priority)) {
+					PostfixNotation.push_back(ops.top());
+					ops.pop();
+				}
+				ops.push(token);
+			}
+			else if (IsPredefined(token)) 
+				PostfixNotation.push_back(std::to_string(GetPredefinedValue(token)));
+			
+			else if (IsVariable(token))
+				PostfixNotation.push_back(token);
 			continue;
 			
 		}
@@ -173,30 +226,117 @@ void Calculate(const std::string& op) {
 		long double a = GetParameter();
 		Results.push(std::pow(a, b));
 	}
-	else if (op == "=") {
-		long double b = GetParameter();
-		long double a = GetParameter();
-		Results.push(b);
+	else if (op == "\\") {
+		long B = (long)GetParameter();
+		long A = (long)GetParameter();
+
+		Results.push((long)(A - (B * (long)(A / B))));
 	}
-	else if (op == "==") {
-		long double b = GetParameter();
-		long double a = GetParameter();
-		Results.push(a == b);
+	else if (op == "mod") {
+		long double B = GetParameter();
+		long double A = GetParameter();
+
+		Results.push(A - (B * (long)((long)A / (long)B)));
+	}
+	else if (op == "=") {
+		long double B = GetParameter();
+		long double A = GetParameter();
+
+		Results.push(A == B);
+	}
+	else if (op == "<>") {
+		long double B = GetParameter();
+		long double A = GetParameter();
+
+		Results.push(A != B);
+	}
+	else if (op == "<") {
+		long double B = GetParameter();
+		long double A = GetParameter();
+
+		Results.push(A < B);
+	}
+	else if (op == "<=") {
+		long double B = GetParameter();
+		long double A = GetParameter();
+
+		Results.push(A <= B);
+	}
+	else if (op == ">") {
+		long double B = GetParameter();
+		long double A = GetParameter();
+
+		Results.push(A > B);
+	}
+	else if (op == ">=") {
+		long double B = GetParameter();
+		long double A = GetParameter();
+
+		Results.push(A >= B);
+	}
+	else if (op == "not") {
+		long double A = GetParameter();
+
+		Results.push(!A);
+	}
+	else if (op == "and") {
+		long double B = GetParameter();
+		long double A = GetParameter();
+
+		Results.push(A && B);
+	}
+	else if (op == "or") {
+		long double B = GetParameter();
+		long double A = GetParameter();
+
+		Results.push(A || B);
+	}
+	else if (op == "xor") {
+		long double B = GetParameter();
+		long double A = GetParameter();
+		if (A && B)
+			Results.push(0);
+		else if ((A && !B) || (!A && B))
+			Results.push(1);
+		else
+			Results.push(0);
 	}
 	else if (op == "pow") {
 		long double b = GetParameter();
 		long double a = GetParameter();
 		Results.push(std::pow(a, b));
 	}
-	
+	else if (op == "sin") {
+		long double a = GetParameter();
+		Results.push(std::sin(a));
+	}
 }
 
 long double Evaluate() {
 	for (auto it = PostfixNotation.begin(); it != PostfixNotation.end(); ++it) {
 		if (CheckOperator(*it) || IsFunction(*it))
 			Calculate(*it);
-		else if (IsNumber(*it)){
+		else if (IsNumber(*it)) {
 			Results.push(std::stold(*it));
+		}
+		else if (IsVariable(*it)) {
+			long double A;
+			if (GetVariable(*it, A))
+				Results.push(A);
+			else {
+				std::string token;
+				std::cout << "What is the value of " << *it << " ?\n";
+				std::getline(std::cin, token);
+				if (IsNumber(token)) {
+					A = std::stold(token);
+					Results.push(A);
+					AddVariable(*it, A);
+				}
+				else if (IsPredefined(token)) {
+					A = GetPredefinedValue(token);
+					Results.push(A);
+				}
+			}
 		}
 		else {
 			std::string szError("Invalid value " + *it);
@@ -209,24 +349,21 @@ long double Evaluate() {
 //===============Main===============//
 
 int main() {
-	std::string szExpression, szToken;
-
-	std::cout << "1. Use space between numbers and operators (it must be like this so the program can use both positive and negative numbers).\n2. Use only '(' and ')' for paranthesis.\nEnter the expression that needs to be calculated: \n\n\n\n\n";
-
-	while (std::getline(std::cin, szToken)) {
-		szExpression.append(szToken + " ");
-
-		if (szToken.find('\\') == std::string::npos)
-			break;
-	}
-
-	szExpression.erase(std::remove_if(szExpression.begin(), szExpression.end(), [](const char& c) { return c == '\\'; }), szExpression.end());
-
+	std::string szExpression;
+	std::cout << "1. Use space between numbers and operators (it must be like this so the program can use both positive and negative numbers).\n2. Use only '(' and ')' for paranthesis.\n3. For a better result, please use paranthesis where is the case.\nEnter the expression that needs to be calculated:\n\n\n";
+	std::getline(std::cin, szExpression);
 	std::cout << "Calculating:\n" << szExpression << '\n';
 
 	try
 	{
 		GeneratePostfix(szExpression);
+
+		std::cout << "The postfix form of the expression is: ";
+
+		for (const auto& p : PostfixNotation)
+			std::cout << p << ' ';
+		
+		std::cout << '\n';
 
 		std::cout << "Result: " << Evaluate() << '\n';
 	}
